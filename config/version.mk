45c46e148613160f1304c445869e0f32faf69dfa
Rajkale99 <kaleraj.rk@gmail.com>
2020-09-24 19:15:17 +0000

    Bump to v3.1

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 11.0
-LEGIONVERSION := v3.0
+LEGIONVERSION := v3.1
 
 LEGION_BUILD_TYPE ?= Unofficial
 LEGION_BUILD_DATE := $(shell date +%Y%m%d)
